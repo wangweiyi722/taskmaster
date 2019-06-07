32fadcc7b8d2b3f672f797f2e00e36df60504b03
Weiyi Wang <weiyiwang@google.com>
2019-06-07 11:06:33 -0500

    added basic Event component git status

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Event from "./Event";
+
+class App extends React.Component{
+
+  render(){
+    return(
+      <div>
+        <Event/>
+      </div>
+    )
+  }
+
+}
+
+export default App;
