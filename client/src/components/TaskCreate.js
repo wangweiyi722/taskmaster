bb452b7b3801cd557d90bfa12f37c2d0eaa00afb
Weiyi Wang <weiyiwang@google.com>
2019-07-17 12:30:12 -0500

    beginning to make task create form

--- client/src/components/TaskCreate.js
+++ client/src/components/TaskCreate.js
@@ -2,8 +2,27 @@ import React from 'react';
 import {Field, reduxForm} from 'redux-form';
 
 class TaskCreate extends React.Component {
+
+  renderInput(formProps){
+    //need to take relevant properties out of formProps and assign onChange and value properties
+    return (
+      <div>
+        <label>{formProps.label}</label>
+        <input {...formProps.input}/>
+      </div>
+
+    );
+  }
+
   render(){
-    return <div>TaskCreate</div>;
+    return (
+      <form className="ui form">
+        <Field name="description" component={this.renderInput} label="Enter Description"/>
+        <Field name="deadline" component={this.renderInput} label="Enter Deadline"/>
+        <Field name="assignee" component={this.renderInput} label="Enter Assignee"/>
+
+      </form>
+    );
   }
 }
 
